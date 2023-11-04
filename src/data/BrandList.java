81824c3ed6462383e77c4abacad81739fe7191b2
daidia2004x <149600446+daidia2004x@users.noreply.github.com>
2023-11-04 13:14:03 +0700

    Update BrandList.java

--- src/data/BrandList.java
+++ src/data/BrandList.java
@@ -1,17 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package data;
-
-/**
- *
- * @author 84362
- */
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Collections;
 import java.util.*;
 import java.lang.*;
+
+
 
 public class BrandList extends ArrayList<Brand> {
 
